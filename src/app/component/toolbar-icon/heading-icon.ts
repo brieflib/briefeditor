0f170f7454f95e0e9a5f9bbc882b7ff5de1cf8ee
brieflib <aptsukerman@yandex.ru>
2026-09-13 16:46:27 +0500

    Adjust image control sizes

--- src/app/component/toolbar-icon/heading-icon.ts
+++ src/app/component/toolbar-icon/heading-icon.ts
@@ -15,13 +15,21 @@ class HeadingIcon extends HTMLElement implements Icon {
         super();
         const shadowRoot = initShadowRoot(this, toolbarIconCss);
 
-        for (let i = 0; i <= 5; i++) {
-            shadowRoot.innerHTML += `
-              <button type="button" class="icon ${i === 0 ? "" : "hidden"}" id="button${i}">
-                  <svg viewBox="0 3 22 22" xmlns="http://www.w3.org/2000/svg">
+        shadowRoot.innerHTML = `
+              <button type="button" class="icon hidden" id="button0">
+                  <svg viewBox="1 3 22 22" xmlns="http://www.w3.org/2000/svg">
                     <path class="icon-svg" d="M7 5C7.55228 5 8 5.44772 8 6V11.5H16V6C16 5.44772 16.4477 5 17 5C17.5523 5 18 5.44772 18 6V12.5V19C18 19.5523 17.5523 20 17 20C16.4477 20 16 19.5523 16 19V13.5H8V19C8 19.5523 7.55228 20 7 20C6.44772 20 6 19.5523 6 19V12.5V6C6 5.44772 6.44772 5 7 5Z"/>      
                   </svg>
-                  <sub class="heading-number">${i === 0 ? "" : i}</sub>
+              </button>
+            `;
+
+        for (let i = 1; i <= 5; i++) {
+            shadowRoot.innerHTML += `
+              <button type="button" class="icon ${i === 0 ? "" : "hidden"}" id="button${i}">
+                  <svg viewBox="4 3 22 22" xmlns="http://www.w3.org/2000/svg">
+                    <path class="icon-svg" d="M7 5C7.55228 5 8 5.44772 8 6V11.5H16V6C16 5.44772 16.4477 5 17 5C17.5523 5 18 5.44772 18 6V12.5V19C18 19.5523 17.5523 20 17 20C16.4477 20 16 19.5523 16 19V13.5H8V19C8 19.5523 7.55228 20 7 20C6.44772 20 6 19.5523 6 19V12.5V6C6 5.44772 6.44772 5 7 5Z"/>      
+                  </svg>
+                  <sub class="heading-number">${i}</sub>
               </button>
             `;
         }
